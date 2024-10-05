c7cad59b478ee46fc1a85d96d5b44bda9c93a37b
Paul van der Meijs <vandermeijs@redkiwi.nl>
2024-10-05 10:40:49 +0200

    Render basic list output

--- crates/memos-cli/src/commands/list.rs
+++ crates/memos-cli/src/commands/list.rs
@@ -5,6 +5,15 @@ use crate::auth::Auth;
 
 pub(crate) fn list(auth: Auth) -> Result<()> {
     let result = memo_service_list_memos(&auth.try_into()?, None, None, None)?;
+    let output = result
+        .memos
+        .unwrap()
+        .into_iter()
+        .map(|memo| memo.content.unwrap())
+        .collect::<Vec<String>>()
+        .join("\n\n---\n\n");
+
+    println!("{output}");
 
     Ok(())
 }
